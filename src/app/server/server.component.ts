ff950c547bad852f9231bcfd2cc2d7d97ce6e73c
JackTattersall <jack.tattersall@opencastsoftware.com>
2017-07-29 16:55:38 +0100

    Data-bindings

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,8 +2,23 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
+  serverId = 5;
+  serverStatus = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
+  }
+
+  // has to resolve (return) to a string to be used in html {{ }}
+  getserverStatus() {
+    return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = true;
+  serverCreationStatus = 'No server was created';
+  serverName = '';
+  serverCreated = false;
+  servers = ['Test Server', 'Prod Server'];
 
-  constructor() { }
+  constructor() {
+    // when this component is constructed waits 2 seconds then changes allowNewServer
+    setTimeout(() => {
+      this.allowNewServer = false;
+    }, 2000);
+  }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+  }
 }
